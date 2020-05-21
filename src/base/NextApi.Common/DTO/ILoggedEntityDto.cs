cc760d36e206a11da42b0ca1db7609255c8ad31d
lordofffarm <omarbekov.e@gmail.com>
2020-05-21 10:22:52 +0600

    ILoggedEntityDto (#20)
    Co-authored-by: omarbekov <omarbekov@abitech.kz>

--- src/base/NextApi.Common/DTO/ILoggedEntityDto.cs
+++ src/base/NextApi.Common/DTO/ILoggedEntityDto.cs
@@ -4,13 +4,15 @@ namespace NextApi.Common.DTO
 {
     /// <summary>
     /// Interface represents entity to be logged
+    /// <remarks>WARNING! If you are going to use generic arguments other than string,
+    /// you will have to override NextApiDbContext.RecordAuditInfo</remarks>
     /// </summary>
-    public interface ILoggedEntityDto
+    public interface ILoggedEntityDto<TUserIdType>
     {
         /// <summary>
         /// Contains id of user created this entity
         /// </summary>
-        int? CreatedById { get; set; }
+        TUserIdType CreatedById { get; set; }
 
         /// <summary>
         /// Contains creation time
@@ -20,11 +22,18 @@ namespace NextApi.Common.DTO
         /// <summary>
         /// Contains id of user updated this entity last time
         /// </summary>
-        int? UpdatedById { get; set; }
+        TUserIdType UpdatedById { get; set; }
 
         /// <summary>
         /// Contains time when entity last time updated
         /// </summary>
         DateTimeOffset? Updated { get; set; }
+    }
+
+    /// <summary>
+    /// Interface represents entity to be logged with string user id property type
+    /// </summary>
+    public interface ILoggedEntityDto : ILoggedEntityDto<string>
+    {
     }
 }
