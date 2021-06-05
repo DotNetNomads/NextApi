0e3f73122223a602b87220f9ed300d0b9f61d786
Asylkhan Azat <asylkhan.azat@gmail.com>
2021-06-05 15:04:52 +0600

    Refactored NextApi.Server/Base/NextApiHttp

---
+++ src/server/NextApi.Server/Base/ICommandArgsExtractor.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using NextApi.Common;
+
+namespace NextApi.Server.Base
+{
+    /// <summary>
+    /// Interface is a contract for extracing NextApi command arguments from <see cref="IFormCollection"/>.
+    /// </summary>
+    internal interface ICommandArgsExtractor
+    {
+        /// <summary>
+        /// Extracts NextApi command arguments from form.
+        /// </summary>
+        /// <param name="form"> Http forms collection. </param>
+        /// <returns> Array of NextApi command arguments. </returns>
+        Task<INextApiArgument[]> Extract(IFormCollection form);
+    }
+}
---
+++ src/server/NextApi.Server/Base/MessagePackCommandResultWriter.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using MessagePack;
+using Microsoft.AspNetCore.Http;
+using NextApi.Common;
+using NextApi.Server.Service;
+
+namespace NextApi.Server.Base
+{
+    /// <summary>
+    /// Writes NextApi response to HttpResponse as MessagePack encoded byte array.
+    /// </summary>
+    internal class MessagePackCommandResultWriter : ICommandResultWriter
+    {
+        /// <inheritdoc/>
+        public async Task Write(HttpResponse httpResponse, INextApiResponse commandResult)
+        {
+            await httpResponse.SendByteArray(MessagePackSerializer.Serialize(commandResult));
+        }
+    }
+}
