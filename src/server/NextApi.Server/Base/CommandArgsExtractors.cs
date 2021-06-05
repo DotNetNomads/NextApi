70f45a6123d1288bb7ed408205c6f5fbd9a2ea20
Asylkhan Azat <asylkhan.azat@gmail.com>
2021-06-05 15:04:52 +0600

    Refactored NextApi.Server/Base/NextApiHttp

---
+++ src/server/NextApi.Server/Base/CommandArgsExtractors.cs
@@ -0,0 +1,19 @@
+﻿using NextApi.Common.Serialization;
+
+namespace NextApi.Server.Base
+{
+    /// <summary>
+    /// Factory class for creating <see cref="ICommandArgsExtractor"/> for specific <see cref="SerializationType"/>.
+    /// </summary>
+    internal static class CommandArgsExtractors
+    {
+        public static ICommandArgsExtractor Create(SerializationType serializationType)
+        {
+            return serializationType switch
+            {
+                SerializationType.MessagePack => new MessagePackCommandArgsExtractor(),
+                _ => new JsonCommandArgsExtractor()
+            };
+        }
+    }
+}
---
+++ src/server/NextApi.Server/Base/CommandResultWriters.cs
@@ -0,0 +1,26 @@
+﻿using NextApi.Common;
+using NextApi.Common.Serialization;
+
+namespace NextApi.Server.Base
+{
+    /// <summary>
+    /// Factory class for creating <see cref="ICommandResultWriter"/> for <see cref="INextApiResponse"/> and <see cref="SerializationType"/>.
+    /// </summary>
+    internal static class CommandResultWriters
+    {
+        public static ICommandResultWriter Create(INextApiResponse response, SerializationType serializationType)
+        {
+            if (response is NextApiFileResponse)
+            {
+                return new FileCommandResultWriter();
+            }
+
+            return serializationType switch
+            {
+                SerializationType.MessagePack => new MessagePackCommandResultWriter(),
+
+                _ => new JsonCommandResultWriter()
+            };
+        }
+    }
+}
